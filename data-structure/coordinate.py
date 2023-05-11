e7928ba485abd6b976d7edcdd5495e147ea54ef5
Leonam Knupp <leonamknupp@hotmail.com>
2023-05-10 22:11:58 -0300

    Refactor to use dataclass decorator

--- data-structure/coordinate.py
+++ data-structure/coordinate.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
+@dataclass
 class Coordinate:
-    def __init__(self, x_coordinate: int, y_coordinate: int) -> None:
-        self.x_coordinate: int = x_coordinate
-        self.y_coordinate: int = y_coordinate
+    x_coordinate: int
+    y_coordinate: int
